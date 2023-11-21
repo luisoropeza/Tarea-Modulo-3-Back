f14b2658fe7133c02d43cb5a94124f672e2883d1
luisoropeza <luisdavid.ingsis@gmail.com>
2023-11-21 07:11:08 -0400

    realizando alguno cambios para el metodo getuserwithrolbyname

--- src/main/java/com/diplomado/demo/repositories/data/jpa/RolRepository.java
+++ src/main/java/com/diplomado/demo/repositories/data/jpa/RolRepository.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import com.diplomado.demo.models.RolEntity;
 
+
 @Repository
 public interface RolRepository extends JpaRepository<RolEntity, Long> {
-    
+    RolEntity findByName(String name);
 }
